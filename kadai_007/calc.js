289030824482c6c8d0a24da96bbeadae318a1aa4
yuzuka-side-0107 <153477819+yuzuka-side-0107@users.noreply.github.com>
2023-12-16 16:35:24 +0900

    修正版をアップロード

--- kadai_007/calc.js
+++ kadai_007/calc.js
@@ -1,18 +1,19 @@
-let num = Math.floor(Math.random() * 16);
+let num = Math.floor(Math.random() * 100);
 
-if (num === 3 || num === 6 || num === 9 || num === 12) {
+console.log(num);
+
+if (num % 3 === 0 && num % 5 === 0) {
+  console.log('3と5の倍数です');
+}
+
+else if (num % 3 === 0) {
   console.log('3の倍数です');
 }
 
-else if (num === 5 || num === 10) {
+else if (num % 5 === 0) {
   console.log('5の倍数です');
-}
-
-else if (num === 15) {
-  console.log('3と5の倍数です');
 }
 
 else {
   console.log(num);
 }
-
